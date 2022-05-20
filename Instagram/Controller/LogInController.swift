55ee74a9d9fa47f856caf792709a660e28821128
oradwanomar <oradwan037@gmail.com>
2022-05-20 21:53:48 +0200

    create log in page

---
+++ Instagram/Controller/LogInController.swift
@@ -0,0 +1,12 @@
+//
+//  LogInController.swift
+//  Instagram
+//
+//  Created by Omar Ahmed on 20/05/2022.
+//
+
+import UIKit
+
+class LogInController : UIViewController {
+    
+}
---
+++ Instagram/Controller/SighUpController.swift
@@ -0,0 +1,12 @@
+//
+//  SighUpController.swift
+//  Instagram
+//
+//  Created by Omar Ahmed on 20/05/2022.
+//
+
+import UIKit
+
+class SighUpController : UIViewController {
+    
+}
