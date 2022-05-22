a0cb896f13328d9525b03f7d91fe35cc7b310204
oradwanomar <oradwan037@gmail.com>
2022-05-22 07:31:32 +0200

    init API models

---
+++ Instagram/API/AuthService.swift
@@ -0,0 +1,8 @@
+//
+//  AuthService.swift
+//  Instagram
+//
+//  Created by Omar Ahmed on 22/05/2022.
+//
+
+import Foundation
---
+++ Instagram/API/ImageUploader.swift
@@ -0,0 +1,12 @@
+//
+//  ImageUploader.swift
+//  Instagram
+//
+//  Created by Omar Ahmed on 22/05/2022.
+//
+
+import FirebaseStorage
+
+struct ImageUploader {
+    
+}
