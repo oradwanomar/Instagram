5055edff5090d2f0c32d50a4904002c6f2c14210
oradwanomar <oradwan037@gmail.com>
2022-05-29 05:36:56 +0200

    imageUploader

--- Instagram/API/ImageUploader.swift
+++ Instagram/API/ImageUploader.swift
@@ -6,7 +6,23 @@
 //
 
 import FirebaseStorage
+import UIKit
 
 struct ImageUploader {
-    
+    static func imageUploader(image:UIImage,completion:@escaping (String)->()){
+        guard let imageData = image.jpegData(compressionQuality: 0.75) else {return}
+        let fileName = NSUUID().uuidString
+        let ref = Storage.storage().reference(withPath: "/profile_images/\(fileName)")
+        ref.putData(imageData, metadata: nil) { metadata, error in
+            if let error = error {
+                print("Error : Failed to upload image \(error.localizedDescription)")
+                return
+            }
+            ref.downloadURL { url, error in
+                guard let imageUrl = url?.absoluteString else {return}
+                completion(imageUrl)
+            }
+        }
+        
+    }
 }
