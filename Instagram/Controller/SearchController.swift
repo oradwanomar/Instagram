807dd14375620d88277f214312d2ee2f8b9865b0
oradwanomar <oradwan037@gmail.com>
2022-05-31 08:08:06 +0200

    init search table view

--- Instagram/Controller/SearchController.swift
+++ Instagram/Controller/SearchController.swift
@@ -7,11 +7,27 @@
 
 import UIKit
 
-
-class SearchController : UIViewController {
+private let reuseIdentifier = "usercell"
+class SearchController : UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureTableView()
+    }
+    
+    func configureTableView(){
         view.backgroundColor = .systemBackground
+    }
+}
+
+extension SearchController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        return cell
     }
 }
---
+++ Instagram/View/UserCell.swift
@@ -0,0 +1,14 @@
+//
+//  UserCell.swift
+//  Instagram
+//
+//  Created by Omar Ahmed on 31/05/2022.
+//
+
+import Foundation
+import UIKit
+
+class UserCell : UITableViewCell {
+    
+    
+}
