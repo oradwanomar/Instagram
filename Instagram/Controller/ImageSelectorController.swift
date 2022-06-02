5bc35ad7d84c8db5fde2325933179acf1ec6aa2c
oradwanomar <oradwan037@gmail.com>
2022-06-02 10:39:51 +0200

    fix

--- Instagram/Controller/ImageSelectorController.swift
+++ Instagram/Controller/ImageSelectorController.swift
@@ -14,6 +14,5 @@ class ImageSelectorController : UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        
     }
 }
