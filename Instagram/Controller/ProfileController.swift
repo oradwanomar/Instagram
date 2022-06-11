9813b5a5cc3930c293ea7cc294fb944633e09308
omarradwan <oradwan037@gmail.com>
2022-06-11 20:53:59 +0200

    Update ProfileController.swift

--- Instagram/Controller/ProfileController.swift
+++ Instagram/Controller/ProfileController.swift
@@ -109,7 +109,6 @@ extension ProfileController : UICollectionViewDelegateFlowLayout {
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
         return CGSize(width: view.frame.width, height: 240)
-
     }
 }
 
