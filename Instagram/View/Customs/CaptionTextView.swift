284bc33540273dcb1168b8b4a0e496861aff4a57
omarradwan <oradwan037@gmail.com>
2022-06-12 09:29:06 +0200

    Update CaptionTextView.swift

--- Instagram/View/Customs/CaptionTextView.swift
+++ Instagram/View/Customs/CaptionTextView.swift
@@ -9,4 +9,13 @@ import UIKit
 
 class CaptionTextView : UITextView {
     
+    
+    
+    override init(frame: CGRect, textContainer: NSTextContainer?){
+        super.init(frame: frame, textContainer: textContainer)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
