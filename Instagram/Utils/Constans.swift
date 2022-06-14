04b2e3769bf28171fc5cdf0a4a2d757cb4c6c885
omarradwan <oradwan037@gmail.com>
2022-06-14 08:55:52 +0200

    Update Constans.swift

--- Instagram/Utils/Constans.swift
+++ Instagram/Utils/Constans.swift
@@ -10,3 +10,4 @@ import Firebase
 let COLLECTION_USERS = Firestore.firestore().collection("users")
 let COLLECTION_FOLLOWING = Firestore.firestore().collection("following")
 let COLLECTION_FOLLOWERS = Firestore.firestore().collection("followers")
+let COLLECTION_POSTS = Firestore.firestore().collection("posts")
