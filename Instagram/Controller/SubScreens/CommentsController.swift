c581a367a10f5b6780a6d7fa0e0b039d636c6ec8
omarradwan <oradwan037@gmail.com>
2022-06-26 05:43:14 +0200

    make changes

---
+++ Instagram/Controller/SubScreens/CommentsController.swift
@@ -0,0 +1,57 @@
+//
+//  CommentsController.swift
+//  Instagram
+//
+//  Created by Omar Ahmed on 26/06/2022.
+//
+
+import UIKit
+
+private let reuseIdentifier = "CommentsCell"
+
+class CommentsController: UICollectionViewController {
+    
+    //MARK: properities
+    
+    
+    //MARK: lifeCycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+
+
+}
+
+// MARK: UICollectionViewDataSource
+
+extension CommentsController {
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 0
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        return cell
+    }
+}
+
+// MARK: UICollectionViewDelegate
+
+extension CommentsController {
+    
+}
+
+//MARK: UICollectionViewDelegateFlowLayout
+
+extension CommentsController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 80)
+    }
+}
---
+++ Instagram/View/Cells/CommentCell.swift
@@ -0,0 +1,12 @@
+//
+//  CommentCell.swift
+//  Instagram
+//
+//  Created by Omar Ahmed on 26/06/2022.
+//
+
+import UIKit
+
+class CommentCell: UICollectionViewCell {
+    
+}
