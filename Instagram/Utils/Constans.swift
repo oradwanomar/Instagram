6ebb74abfdab8398699998a52ca72c62a45cf047
omarradwan <oradwan037@gmail.com>
2022-06-27 20:42:40 +0200

    try

--- Instagram/Utils/Constans.swift
+++ Instagram/Utils/Constans.swift
@@ -11,3 +11,4 @@ let COLLECTION_USERS = Firestore.firestore().collection("users")
 let COLLECTION_FOLLOWING = Firestore.firestore().collection("following")
 let COLLECTION_FOLLOWERS = Firestore.firestore().collection("followers")
 let COLLECTION_POSTS = Firestore.firestore().collection("posts")
+
