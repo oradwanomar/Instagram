d23ddfea3795d0ce5e1d1a26a018c486351c6ba0
omarradwan <oradwan037@gmail.com>
2022-07-02 21:50:40 +0200

    Update CommentsController.swift

--- Instagram/Controller/SubScreens/CommentsController.swift
+++ Instagram/Controller/SubScreens/CommentsController.swift
@@ -111,7 +111,6 @@ extension CommentsController {
 
 extension CommentsController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
         let viewModel = CommentsViewModel(comment: comments[indexPath.row])
         
         let height = viewModel.size(forWidth: view.frame.width).height + 32
